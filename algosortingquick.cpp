7dcb2a65c226374349fa1e18eff775b269705289
albert.durdiev <albert.durdiev@laposte.net>
2021-01-02 02:16:07 +0100

    Algo quick pratiquement fini mais à débugger calmement plus tard

--- algosortingquick.cpp
+++ algosortingquick.cpp
@@ -4,5 +4,42 @@ void AlgoSortingQuick::run()
 {
     SetSolvingIsRunning(true);
 
+    solveAlgo(0, currentValues.size()-1);
+
     SetSolvingIsRunning(false);
 }
+
+int AlgoSortingQuick::solvePartition(int beginIndex, int endIndex)
+{
+    int pivot { endIndex };
+    int j { beginIndex - 1 };
+
+    for(int i = beginIndex ; i < endIndex  ; i++)
+    {
+        if(currentValues[i]->Data() <= currentValues[pivot]->Data())
+        {
+            j++;
+            auto buffer = currentValues[i] ;
+            currentValues[i] = currentValues[j];
+            currentValues[j] = buffer;
+        }
+    }
+
+    auto buffer = currentValues[j];
+    currentValues[j] = currentValues[pivot];
+    currentValues[pivot] = buffer;
+
+    return j;
+}
+
+
+void AlgoSortingQuick::solveAlgo(int beginIndex, int endIndex)
+{
+    if(beginIndex < endIndex)
+    {
+        int pivotIndex = solvePartition(beginIndex, endIndex);
+        solveAlgo(beginIndex, pivotIndex-1);
+        solveAlgo(pivotIndex+1, endIndex);
+    }
+}
+
--- algosortingquick.h
+++ algosortingquick.h
@@ -8,6 +8,9 @@ class AlgoSortingQuick : public AlgoSorting
     Q_OBJECT
 public:
     void run() override;
+private :
+    int solvePartition(int beginIndex, int endIndex);
+    void solveAlgo(int beginIndex, int endIndex);
 };
 
 #endif // ALGOSORTINGQUICK_H
